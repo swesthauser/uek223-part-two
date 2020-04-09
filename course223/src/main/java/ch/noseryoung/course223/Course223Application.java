ef0a506de90344737b4010bc1456f0ad819ded73
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2020-04-09 10:04:50 +0200

    spring project added

---
+++ course223/src/main/java/ch/noseryoung/course223/Course223Application.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.course223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course223Application.class, args);
+	}
+
+}
